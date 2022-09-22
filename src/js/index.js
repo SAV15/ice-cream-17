fd529aaba707663fc07a0c64d793eb13e7f208bb
Viktoriia212022 <3derudit@gmail.com>
2022-09-22 16:32:58 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import './modal-loc'
 import './modal-buy'
 import './modal-fr'
-// import './modal-ing'
-// import './modal-read'
+import './modal-ing'
+import './modal-read'
 import "./_mobile-menu-header"
 
 import "./_swiper"
